2a624c546a810c24b39031f477ed1e5d74793206
giwrgos.vasilop@gmail.com <giwrgos.vasilop@gmail.com>
2022-11-21 23:26:26 +0200

    Added the first attempt to create a CloudFront Distribution using Terraform

--- Main.tf
+++ Main.tf
@@ -102,4 +102,47 @@ resource "aws_s3_bucket_website_configuration" "mybucketwebconfig2" {
   }
 }
 
-# 
+# Create a Cloudfront Distribution for both buckets-origins
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  enabled = true
+  aliases = ["gvasilopoulos.xyz", "www.gvasilopoulos.xyz"]
+  price_class = "PriceClass_100"
+  is_ipv6_enabled = true
+  
+  default_cache_behavior {
+    allowed_methods = ["GET", "HEAD"]
+    cached_methods = ["GET", "HEAD"]
+    target_origin_id = aws_s3_bucket.b1.bucket
+    viewer_protocol_policy = "redirect-to-https"
+    compress = true
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+  
+  viewer_certificate {
+    acm_certificate_arn = "arn:aws:acm:us-east-1:786880879176:certificate/bdab6c9b-29a3-453f-bab2-021e2b292c92"
+    ssl_support_method = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2019"
+  }
+  
+  origin {
+    domain_name = aws_s3_bucket.b1.bucket_regional_domain_name
+    origin_id = "tf-s3-website.gvasilopoulos.xyz"
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.s3_distribution.cloudfront_access_identity_path
+    }
+  }
+  origin {
+    domain_name = aws_s3_bucket.b2.bucket_regional_domain_name
+    origin_id = "tf-s3-website.www.gvasilopoulos.xyz"
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.s3_distribution.cloudfront_access_identity_path
+    }
+  }
+}
+
